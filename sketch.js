e2100b2ca4aea5253add3b0717ba8c5aee94e093
Lucasbleyder2010 <100377923+Lucasbleyder2010@users.noreply.github.com>
2022-08-25 18:25:09 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,145 @@
+// meu objetivo é criar um jogo de batalha naval, vão ser 2 navios:
+//Sunny = Jogador
+//Merry = Pc
+//Um fundo do oceano e fazer os barcos jogarem bolas de canhão um no outro.
+// colocar o placar e o jogo vai acabar quando 1 deles for acertado com 3 canhões.
+// não pensei em colocar som
+// me basiei no Archery e no jogo do canhão
+//Escreva o nome do seu Projeto
+
+//R: Boats Battles
+
+//Qual é o objetivo do jogo?
+
+//R: Dois barcos vão se enfrentar até um perder
+
+//Escreva uma breve história sobre o seu jogo.
+
+//R: Vários piratas da tripulação Sunny estavam comemorando e se divertindo bastante
+//até aparecer um outro navio e vários outros piratas, seu navio se chamava Merry
+//os dois se encaravam e derrepente estavam preprando os canhões e tentavam acertar um ao outro
+
+//Quais são os Personagens Jogáveis deste jogo? 
+
+//R: 2barcos: Sunny e Merry
+
+//Personagens jogáveis são aqueles que respondem ao usuário com base na entrada fornecida por ele.
+//Carros, macacos, dinossauros, feiticeiros, etc., são os personagens jogáveis.  
+
+//R: personagens jogaveis: Sunny(você)
+
+
+//Nome do Personagem
+
+//R:Suuny
+
+//O que esse personagem faz?
+
+//R: Um navio que atira uma bola de canhão
+
+//1
+
+
+
+
+2
+
+
+
+
+3
+
+
+
+
+4
+
+
+
+
+5
+
+
+
+
+6
+
+
+
+
+7
+
+
+
+
+8
+
+
+
+
+
+
+//Quais são os personagens não-jogáveis deste jogo?
+
+//R:Merry(computador)
+
+//Personagens não-jogáveis são aqueles que não têm uma ação ou comportamento quando o usuário interage com o jogo.
+//Barreiras, pedras, bananas, moedas, etc., são personagens não-jogáveis.   
+
+//Número = 2
+//Nome do Personagem
+
+//Merry
+
+//O que esse personagem faz?
+1
+//O mesmo que o Sunny, joga uma bola de canhão
+
+
+2
+
+
+
+
+3
+
+
+
+
+4
+
+
+
+
+5
+
+
+
+
+6
+
+
+
+
+7
+
+
+
+
+8
+
+
+
+
+
+
+
+//Use sua imaginação para desenhar esse jogo. Qual é o visual desse jogo?
+
+//R:Um fundo do oceano 
+
+//Desenhe o jogo no seu computador ou no papel.
+//Adicione imagens das cenas do jogo para mostrar cada um dos personagens jogáveis e não-jogáveis pelo menos uma vez. 
+//Como você planeja tornar seu jogo envolvente? 
+
+//R: 
